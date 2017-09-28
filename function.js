5fb4d11139a7517df136c4ad6847697152f91fc5
Henry Wu <henry1016a@gmail.com>
2017-09-28 14:34:55 +0800

    Update function.js

--- function.js
+++ function.js
@@ -1,6 +1,27 @@
 videojs.plugin('plugin_test', function() {
-	// var MainBlock1 = videojs("MainBlock1");
-	console.log('javascript_loading');
+	$(document).ready(function(){
 
+	// SlideDown Display ------------------------------------
+		$("#SlideDown").click(function(){
+			$('.CoverBlockSlideDown').stop().animate({
+				top: '0px'
+			}, 1000);
+		});
+
+		$(".CloseBlockSlideDown").click(function(){
+			$(".CoverBlockSlideDown").animate({
+				top:'-150%'
+			}, 1000);
+		});
+
+	// FadeIn Display ----------------------------------------
+		$("#FadeIn").click(function(){
+			$('.CoverBlockFadeIn').fadeIn('slow/400/fast');
+		});
+		$(".CloseBlockFadeOut").click(function(){
+			$('.CoverBlockFadeIn').fadeOut('slow/400/fast');
+		});
+
+	});
 });
 
